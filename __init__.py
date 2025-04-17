1858c5a30a4fda0ff5b7785beae056d5dfa400fc
Peter Falkingham <pfalkingham@googlemail.com>
2025-04-17 09:38:13 +0100

    updated version number in init.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Range of Motion Finder",
     "author": "Peter Falkingham, Andréas Jannel, Ben Griffin (Liverpool John Moores University)",
-    "version": (1, 5),
+    "version": (1, 6)
     "blender": (4, 4, 0),
     "location": "View3D > Sidebar > ROM",
     "description": "Find poses where two objects collide based on rotations and translations",
