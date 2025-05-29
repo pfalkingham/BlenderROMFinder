9b144ebd3cd02d9acd39baff807886a17cfe2445
Peter Falkingham <pfalkingham@googlemail.com>
2025-05-29 12:49:24 +0100

    version number

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Range of Motion Finder",
     "author": "Peter Falkingham, Andréas Jannel, Ben Griffin (Liverpool John Moores University), and gratuitious AI/LLMs, especiially Google AI Studio",
-    "version": (2, 5, 5),
+    "version": (2, 5, 6),
     "blender": (4, 4, 0),
     "location": "View3D > Sidebar > ROM",
     "description": "Find poses where two objects do not collide based on rotations and translations",
